ba7eb53382817b99c7ae73385246b497230a7511
peteradeojo <peteradeojo@outlook.com>
2020-10-05 18:12:44 +0100

    help

--- src/app.js
+++ src/app.js
@@ -10,8 +10,20 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', {
+	encoding: 'UTF8'
+});
+
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', {
+	encoding: 'UTF8'
+});
+
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-	res.render('index', {title: 'Index'});
+	res.render('index', {title: 'Account Summary', accounts: accounts});
 });
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
