bbe2ea796e4761b7f7b06fcc42e7defed2497676
peteradeojo <peteradeojo@outlook.com>
2020-10-05 19:18:20 +0100

    calculations oooo

--- src/app.js
+++ src/app.js
@@ -45,7 +45,22 @@ app.get('/transfer', (req, res) => {
 });
 
 app.post('/transfer', (req, res) => {
-	const accounts = req.body;
+	accounts[req.body.from] = accounts[req.body.from].balance - req.body.amount;
+	accounts[req.body.to].balance = parseInt(accounts[req.body.from].balance) + parseInt(req.body.amount, 10);
 	
+	const accountsJSON = JSON.stringify(accounts, null, 4);
+	fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+	res.render('transfer', {message: 'Transfer Completed'});
 });
+
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req, res) => {
+	accounts.credit.balance -= req.body.amount;
+	accounts.credit.available += parseInt(req.body.amount);
+
+	const accountsJSON = JSON.stringify(accounts, null, 4);
+	fs.writeFileSync(path.join(__dirname, 'json/accounts.json') , accountsJSON, 'utf8');
+	res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+});
+
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
